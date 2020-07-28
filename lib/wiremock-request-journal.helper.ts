19c8c33bc8efe4ff028d1ba78315b65ac9e53ee0
Roman Valina <roman.valina@citrix.com>
2020-07-28 15:15:46 +0200

    Added support for  - request patterns with file reference  - request verifications  - request pattern builders

--- lib/wiremock-request-journal.helper.ts
+++ lib/wiremock-request-journal.helper.ts
@@ -1,70 +1,79 @@
-import { RequestPattern } from './wiremock-domain';
-import { WiremockHelper } from './wiremock.helper';
+import {RequestPattern} from './wiremock-domain';
+import {WiremockHelper} from './wiremock.helper';
 
 /**
  * Helper for interacting with the request journal of Wiremock (if enabled)
  */
 export class WiremockRequestJournalHelper extends WiremockHelper {
 
-  private readonly browserName: string;
+    private readonly browserName: string;
 
-  constructor(baseUrl?: string, browserName?: string) {
-    super(baseUrl);
-    this.browserName = browserName.charAt(0).toUpperCase() + browserName.toLowerCase().slice(1);
-  }
+    constructor(baseUrl?: string, browserName?: string) {
+        super(baseUrl);
+        this.browserName = browserName.charAt(0).toUpperCase() + browserName.toLowerCase().slice(1);
+    }
 
-  private addBrowserHeader(query: RequestPattern): RequestPattern {
-    return {
-      ...query,
-      headers: {
-        'User-Agent': {
-          contains: this.browserName
-        }
-      }
-    };
-  }
+    private addBrowserHeader(query: RequestPattern): RequestPattern {
+        return {
+            ...query,
+            headers: {
+                'User-Agent': {
+                    contains: this.browserName
+                }
+            }
+        };
+    }
 
-  /**
-   * Get all the received requests
-   */
-  getRequestJournal(): Promise<any> {
-    return this.performRequest({
-      method: 'GET',
-      path: '/__admin/requests'
-    });
-  }
+    /**
+     * Get all the received requests
+     */
+    getRequestJournal(): Promise<any> {
+        return this.performRequest({
+            method: 'GET',
+            path: '/__admin/requests'
+        });
+    }
 
-  /**
-   * Remove all the received requests (log) from Wiremock
-   */
-  resetRequestJournal(): Promise<void> {
-    return this.performRequest({
-      method: 'DELETE',
-      path: '/__admin/requests'
-    });
-  }
+    /**
+     * Remove all the received requests (log) from Wiremock
+     */
+    resetRequestJournal() {
+        return super.resetRequestJournal();
+    }
 
-  /**
-   * Get the number of received requests given the specific query parameters
-   * @param query   The query as specified in http://wiremock.org/docs/api/
-   */
-  getRequestCount(query: any): Promise<any> {
-    return this.performRequest({
-      method: 'POST',
-      path: '/__admin/requests/count'
-    }, JSON.stringify(query));
-  }
+    /**
+     * Retrieves the count the provided {@link RequestPattern requestPattern} was called since the mapping was
+     * configured to Wiremock server or request journal has been reset
+     *
+     * @param requestPattern the match pattern to be verified
+     *
+     * @see RequestPatternBuilder
+     * @see resetRequestJournal
+     */
+    getRequestCount(query: RequestPattern): Promise<number> {
+        return super.performRequest({
+            method: 'POST',
+            path: '/__admin/requests/count'
+        }, JSON.stringify(query))
+            .catch(e => {
+                console.warn("Verification fallback to 0. %s", e);
+                return {count: 0};
+            })
+            .then(result => {
+                return result.count;
+            });
+    }
 
-  /**
-   * Obtains the requests matching the query parameters values
-   * @param query   The RequestPattern object defining the matching rules for the requests
-   * @returns the response from the stub: containing an array with the requests
-   */
-  findRequests(query: RequestPattern): Promise<any> {
-    return this.performRequest({
-      method: 'POST',
-      path: '/__admin/requests/find'
-    }, JSON.stringify(this.addBrowserHeader(query)));
-  }
+    /**
+     * Obtains the requests matching the query parameters values
+     * @param query   The RequestPattern object defining the matching rules for the requests
+     * @returns the response from the stub: containing an array with the requests
+     */
+    findRequests(query: RequestPattern): Promise<any> {
+        return this.performRequest({
+            method: 'POST',
+            path: '/__admin/requests/find'
+        }, JSON.stringify(this.addBrowserHeader(query)));
+    }
 
 }
